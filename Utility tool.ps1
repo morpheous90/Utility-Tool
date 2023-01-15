ab704cdce013ee56e646279927f26c14df3d603f
Manu Sharma <manu231295@gmail.com>
2023-01-15 14:42:17 +0530

    Second commit

--- Utility tool.ps1
+++ Utility tool.ps1
@@ -16,17 +16,11 @@ Author: Morphine
 -Contains Test-Path in IF ELSE for file validation if file doesn't exist.
 -Opens the required file.
 
-More Changes - Instead of going through each inside loop (line no 37), 
-will put a function so its more convenient - still same as below string.
--function was a tad tedious, now the user can put the path of the location in external file (for now txt , csv can be used).
-2) also thinking of splitting the $excel variable w/o ' '- would look coool may be
--splitting wont work, but replace will work., no need will still return a string.
-- no need , Start Process helps.
 ** *** ******
-all was rubbish, used a simple start cmdlet to initiate the process
-start $excel , but since I moved the file to some external file to make it easy for the user. here we are :)/\
+Need to remove hard coded paths and add something for it.
 ---------------######################--------------
 Now implementation is using Teams here to get files and stuff. - For some other day.
+
 
 #>
 
